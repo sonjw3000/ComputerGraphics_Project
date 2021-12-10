20659f163505189db2d1b09734f633a814aa5ee4
Jungwon Desktop <sonjw3000@gmail.com>
2021-12-10 13:21:54 +0900

    temp

--- PPOOTTAALL/PPOOTTAALL/MyInclude/Player.h
+++ PPOOTTAALL/PPOOTTAALL/MyInclude/Player.h
@@ -12,10 +12,10 @@ private:
 	TextureClass* m_pTextureBody;
 	TextureClass* m_pTextureLeg;
 
-	Mesh* m_meBody;
-	Mesh* m_meLeftLeg;
-	Mesh* m_meRightLeg;
-	Mesh* m_meBackLeg;
+	Mesh* m_pBody;
+	Mesh* m_pLeftLeg;
+	Mesh* m_pRightLeg;
+	Mesh* m_pBackLeg;
 public:
 	Player() = delete;
 	Player(float size, glm::vec3 pivot);
